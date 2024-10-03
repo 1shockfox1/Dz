d7bbe710376601a268b2191b75157a2f87dc2347
shockfox <shockfox@209-4>
2024-10-03 09:38:03 +0200

    Добавьте файлы проекта.

---
+++ Dz(2)/Program.cs
@@ -0,0 +1,19 @@
+﻿try
+{
+    Console.WriteLine("Добро пожаловать это простая задача для нахождения частоты)");
+    Console.WriteLine("Введите первую переменную С:");
+    double C = double.Parse(Console.ReadLine());
+    Console.WriteLine("Введите вторую переменную L:");
+    double L = double.Parse(Console.ReadLine());
+    Console.WriteLine("Сначало находим T");
+    double T = 2 * Math.PI * Math.Sqrt(C * L);
+    Console.WriteLine("T = " + T);
+    Console.WriteLine("Теперь найдем частоту");
+    double V = 1 / T;
+    Console.WriteLine(" и наша частота(V) = " + V);
+}
+
+catch
+{
+    Console.WriteLine("эх эх не правильно вел значения....");
+}
